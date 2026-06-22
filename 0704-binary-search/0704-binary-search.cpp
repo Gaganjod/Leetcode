c712242993b90e42752e6e4217c07c500917c45c
Gagan Chaudhary <gaganchaudhary.s2005@gmail.com>
2026-06-22 17:42:59 +0530

    Time: 0 ms (100%), Space: 31.4 MB (7.1%) - LeetHub

--- 0704-binary-search/0704-binary-search.cpp
+++ 0704-binary-search/0704-binary-search.cpp
@@ -7,16 +7,14 @@ public:
 
         while(low<=high){
             int mid=low+(high-low)/2;
-
             if(nums[mid]==target){
                 return mid;
-            }else if(nums[mid]<target){
+            }else if(target>nums[mid]){
                 low=mid+1;
             }else{
                 high=mid-1;
             }
         }
         return -1;
-        
     }
 };
