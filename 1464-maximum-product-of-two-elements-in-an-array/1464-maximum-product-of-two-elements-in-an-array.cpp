9f2f53ff7e8caa1fb8d91ac436d62b4e0dd5c6e6
Gagan Chaudhary <gaganchaudhary.s2005@gmail.com>
2026-07-27 14:19:32 +0530

    Time: 0 ms (100%), Space: 13.6 MB (25.65%) - LeetHub

--- 1464-maximum-product-of-two-elements-in-an-array/1464-maximum-product-of-two-elements-in-an-array.cpp
+++ 1464-maximum-product-of-two-elements-in-an-array/1464-maximum-product-of-two-elements-in-an-array.cpp
@@ -4,9 +4,9 @@ public:
         int n = nums.size();
         int max1 = INT_MIN;
         for (int i = 0; i < n; i++) {
-            for (int j = i+1; j < n; j++) {
+            for (int j = i + 1; j < n; j++) {
                 int val = (nums[i] - 1) * (nums[j] - 1);
-                max1 = max(max1,val);
+                max1 = max(max1, val);
             }
         }
         return max1;
